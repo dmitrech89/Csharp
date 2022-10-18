a9818453a1b3d852948c447276199a7ce271755c
Dmitrii Chubchik <114216460+dmitrech89@users.noreply.github.com>
2022-10-19 00:12:30 +0300

    Add files via upload

---
+++ dz27.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+ Console.Write("Введите число: ");
+ int n = Convert.ToInt32(Console.ReadLine());
+ int result = 0;
+ while (n > 0)
+   {
+     result = result + n % 10;
+     n = n / 10;
+  }
+
+   Console.WriteLine(result);
