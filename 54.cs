a66bc06b52b53f73ddd5c2e3c3c48e1238b28c17
Dmitrii Chubchik <114216460+dmitrech89@users.noreply.github.com>
2022-11-21 00:11:07 +0300

    Add files via upload

---
+++ 54.cs
@@ -0,0 +1,51 @@
+﻿void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
+int[,] linesLength = new int[3, 3];
+FillArray(linesLength);
+PrintArray(linesLength);
+SortArray(linesLength);
+Console.WriteLine();
+PrintArray(linesLength);
---
+++ 56.cs
@@ -0,0 +1,54 @@
+﻿void FillArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] linesLength = new int[4, 4];
+
+void SumElements(int[,] array)
+{
+    int minRow = 0;
+    int minSum = 0;
+    int sumRow = 0;
+    for (int i = 0; i < linesLength.GetLength(1); i++)
+    {
+        minRow = minRow + linesLength[0, i];
+    }
+    for (int i = 0; i < linesLength.GetLength(0); i++)
+    {
+        for (int j = 0; j < linesLength.GetLength(1); j++) sumRow =sumRow + linesLength[i, j];
+        if (sumRow < minRow)
+        {
+            minRow = sumRow;
+            minSum = i;
+        }
+        sumRow = 0;
+    }
+    Console.Write($"{minSum + 1} строка");
+}
+
+
+
+
+FillArrayRandom(linesLength);
+PrintArray(linesLength);
+Console.WriteLine();
+SumElements(linesLength);
