8d38c643439b7d9aeb8b11a43b5110854dc6fe66
Dmitrii Chubchik <114216460+dmitrech89@users.noreply.github.com>
2022-11-23 14:57:17 +0300

    Add files via upload

---
+++ 68.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+
+void AkkermanFunction(int m, int n)
+{
+    Console.Write(Akkerman(m, n)); 
+}
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+AkkermanFunction(m,n);
+
