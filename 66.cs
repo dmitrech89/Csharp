fddbb4d99f906ebeb093096e1385710fd39a0422
Dmitrii Chubchik <114216460+dmitrech89@users.noreply.github.com>
2022-11-23 14:57:41 +0300

    Add files via upload

---
+++ 66.cs
@@ -0,0 +1,21 @@
+﻿
+Console.Clear();
+
+void NumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    NumberSum(numberM, numberN, sum);
+}
+
+Console.WriteLine("Введите число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число M:");
+int numberN = int.Parse(Console.ReadLine());
+
+NumberSum(numberM, numberN, 0);
